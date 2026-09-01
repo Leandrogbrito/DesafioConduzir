48765b8459590b317768ded8367c11863d55c369
Leandro Brito <leandrogomes2002@hotmail.com>
2026-09-01 01:08:56 -0300

    Deploy AWS frontend e backend com configuracao CORS

--- conduzir-frontend/conduzir-frontend/src/api/client.ts
+++ conduzir-frontend/conduzir-frontend/src/api/client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 // Este e o nosso "telefone" central para falar com o backend.
 // baseURL "/api" e redirecionado para http://localhost:8080 pelo proxy do Vite.
 export const api = axios.create({
-  baseURL: '/api',
+ baseURL: 'http://desafio-conduzir-env.eba-ipu5dsfz.us-east-1.elasticbeanstalk.com',
   headers: { 'Content-Type': 'application/json' },
 })
